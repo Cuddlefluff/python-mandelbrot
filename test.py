b4776ead73aaaef9d3cb11cf288fd011fa75a988
unknown <horten@kodeklubben.no>
2018-03-22 19:03:29 +0100

    Added graphics output

--- test.py
+++ test.py
@@ -1,19 +1,26 @@
+from PIL import Image, ImageColor
 
-for px in range(0, 10):
-    for py in range(0, 10):
+width = 1920
+height = 1080
+destinationImage = Image.new("RGB", (width, height))
 
-        x0 = ((px / 10.0) * 3.5) - 2.5
-        y0 = ((py / 10.0) * 2.0) - 1.0
+for px in range(0, width):
+    for py in range(0, height):
+
+        x0 = (((px / width ) * 3.5) - 2.5)
+        y0 = (((py / height) * 2.0) - 1.0)
         x = 0.0
         y = 0.0
         iteration = 0
 
-        while x*x + y*y < 4 and iteration < 1000:
+        while x*x + y*y < 4 and iteration < 1024:
             xtemp = x * x - y * y + x0
             y = 2 * x * y + y0
             x = xtemp
             iteration = iteration + 1
         
         if iteration > 0:
-            color = iteration
-            print(color)
+            color = ((iteration / 1024) * 255)
+            destinationImage.putpixel((px, py), int(color))
+
+destinationImage.save("output.png")
